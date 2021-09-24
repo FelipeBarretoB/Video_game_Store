388facdcebb0e6c67d57836b40d5098afdd06a6a
Martin Cifuentes <marcisego@gmail.com>
2021-09-24 08:21:52 -0500

    primera version funcional de la interfaz

--- src/ui/VideoGameStoreGUI.java
+++ src/ui/VideoGameStoreGUI.java
@@ -22,15 +22,19 @@ public class VideoGameStoreGUI {
 	
 	private String inCase;
 	
-	private String shelves;
+	private String shelves = "";
 	
-	private String clients;
+	private String clients = "";
 	
-	private int totalAmount;
+	private String totalGames = "";
 	
-	private int clientsAmount;
+	private int cashiers = 0;
 	
-	private int shelvesAmount;
+	private int totalAmount = 0;
+	
+	private int clientsAmount = 0;
+	
+	private int shelvesAmount = 0;
 	
 	//strart
 	public void loadMainPage(){
@@ -41,6 +45,15 @@ public class VideoGameStoreGUI {
 	private Pane mainPane;
 	
 	//CreateCase
+	
+	@FXML
+    private Label labFinalCase;
+	
+	@FXML
+    private Label labShelvesAmount;
+
+    @FXML
+    private Label labClientsAmount;
 	
 	@FXML
     private ListView<String> lvShelves;
@@ -56,27 +69,49 @@ public class VideoGameStoreGUI {
 
     @FXML
     private ListView<String> lvClients;
+    
+    @FXML
+    void update(ActionEvent event) {
+    	if(!txtCashierAmount.getText().equals("")) {
+    		cashiers = Integer.parseInt(txtCashierAmount.getText());
+    	}
+    	loadPage("createCase.fxml");
+    	labShelvesAmount.setText(String.valueOf(shelvesAmount));
+    	labClientsAmount.setText(String.valueOf(clientsAmount));
+    	labFinalCase.setText(inCaseProgress());
+    	txtCashierAmount.setText(String.valueOf(cashiers));
+    }
 
     @FXML
     void addCase(ActionEvent event) {
-    	inCase = txtCashierAmount.getText() + "\n" + shelvesAmount +
+    	inCase = cashiers + "\n" + shelvesAmount +
     			"\n" + shelves + "\n" + clientsAmount + "\n" + clients;
     	
     	total += inCase;
+    	totalAmount++;
     }
 
     @FXML
     void finish(ActionEvent event) {
     	total = totalAmount + total;
+    	System.out.println(total);
     }
 
     @FXML
     void openAddClient(ActionEvent event) {
+    	if(!txtCashierAmount.getText().equals("")) {
+    		cashiers = Integer.parseInt(txtCashierAmount.getText());
+    	}
     	loadPage("createClients.fxml");
+    	labAbleGames.setText(totalGames);	
+    	
     }
 
     @FXML
     void openAddShelve(ActionEvent event) {
+    	if(!txtCashierAmount.getText().equals("")) {
+    		cashiers = Integer.parseInt(txtCashierAmount.getText());
+    	}
     	loadPage("createShelves.fxml");
     }
     
@@ -89,17 +124,30 @@ public class VideoGameStoreGUI {
     private TextField txtClientGames;
 
     @FXML
-    private ListView<String> lvGamesInClient;
+    private Label labAbleGames;
     
     @FXML
     void addClient(ActionEvent event) {
     	clients += "\n" + txtClientId.getText() + " " + txtClientGames.getText();
     	clientsAmount++;
+    	txtClientId.setText("");
+    	txtClientGames.setText("");
     }
 
     @FXML
     void back(ActionEvent event) {
     	loadPage("createCase.fxml");
+    	labShelvesAmount.setText(String.valueOf(shelvesAmount));
+    	labClientsAmount.setText(String.valueOf(clientsAmount));
+    	labFinalCase.setText(inCaseProgress());
+    	txtCashierAmount.setText(String.valueOf(cashiers));
+    }
+    
+    public String inCaseProgress() {
+    	String s = "";
+    	s = cashiers + "\n" + shelvesAmount +
+    			"\n" + shelves + "\n" + clientsAmount + "\n" + clients;
+    	return s;
     }
     
     //createShelves
@@ -117,12 +165,17 @@ public class VideoGameStoreGUI {
     void addShelve(ActionEvent event) {
     	String shelve = "";
     	String[] games = txtShelveGames.getText().split("\n");
-    	shelve = txtShelveName.getText() + " " + txtShelveName.getText();
+    	shelve = txtShelveName.getText() + " " + txtShelveFloors.getText();
     	for(int i = 0; i < games.length ;i++) {
-    		shelve += "\n" + games[0];
+    		shelve += "\n" + games[i];
+    		totalGames += "\n" + games[i];
     	}
-    	inCase += shelve;
+    	shelves += "\n" + shelve;
     	shelvesAmount++;
+    	
+    	txtShelveName.setText("");
+    	txtShelveFloors.setText("");
+    	txtShelveGames.setText("");
     }
 	
     public void loadPage(String page) {
