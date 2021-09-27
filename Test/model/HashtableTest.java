7b70976fb02eeb91c658efc73a448cea0a4f9e7c
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2021-09-26 20:31:03 -0500

    Hastable test added

--- Test/model/HashtableTest.java
+++ Test/model/HashtableTest.java
@@ -1,5 +1,7 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class HashtableTest {
@@ -9,8 +11,51 @@ public class HashtableTest {
 		
 	}
 	
+	public void setupScenario2() {
+		table = new Hashtable(153, 25000, 9);
+	}
+	
 	@Test
-	public void testSomething() {
-		
+	public void testHashtable() {
+		setupScenario1();
+		int key = 0;
+		double price = 0;
+		int quantity = 0;
+		table = new Hashtable(key,price,quantity);
+		assertEquals(key, table.getKey());
+		assertEquals(price, table.getPrice());
+		assertEquals(quantity, table.getQuantity());
+	}
+	
+	@Test
+	public void testSetPrice() {
+		setupScenario2();
+		double priceToChange = 15000;
+		//double oldPrice = table.getPrice();
+		table.setPrice(priceToChange);
+		assertEquals(priceToChange, table.getPrice());
+	}
+	
+	@Test
+	public void testSetQuantity() {
+		setupScenario2();
+		int quantity = 12;
+		table.setQuantity(quantity);
+		assertEquals(quantity, table.getQuantity());
+	}
+	
+	@Test
+	public void testSetKey() {
+		setupScenario2();
+		int key = 908;
+		table.setKey(key);
+		assertEquals(key, table.getKey());
+	}
+	
+	@Test
+	public void testPrint() {
+		setupScenario2();
+		String text = "153 25000.0 9";
+		assertEquals(text, table.print());
 	}
 }
